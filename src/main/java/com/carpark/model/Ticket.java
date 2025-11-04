2e7d2019c6a22b64e14c8669f7eb37590bfc46ac
Ahmed Wahba <ahmed.wahba@example.com>
2025-11-04 00:28:26 +0000

    Add custom toString to Ticket to format time to seconds

--- src/main/java/com/carpark/model/Ticket.java
+++ src/main/java/com/carpark/model/Ticket.java
@@ -3,4 +3,14 @@ package com.carpark.model;
 import java.time.LocalDateTime;
 
 public record Ticket(String registrationNumber, int slotNumber, LocalDateTime entryTime, LocalDateTime exitTime, Money fee) {
+    @Override
+    public String toString() {
+        return "Ticket[" +
+               "registrationNumber=" + registrationNumber + ", " +
+               "slotNumber=" + slotNumber + ", " +
+               "entryTime=" + entryTime.withNano(0) + ", " +
+               "exitTime=" + exitTime.withNano(0) + ", " +
+               "fee=" + fee +
+               "]";
+    }
 }
